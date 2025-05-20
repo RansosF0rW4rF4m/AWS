efa254ebdd1778738cce078340bb45812995c5f7
RansosF0rW4rF4m <riquelmenascimento324@gmail.com>
2025-05-20 20:25:54 -0300

    004

--- taskides/settings.py
+++ taskides/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+taskides_config = os.path.join(BASE_DIR, 'taskides.config')
 
 
 # Quick-start development settings - unsuitable for production
